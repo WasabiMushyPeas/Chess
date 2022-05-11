197db85733ff01e0174c1bb54ea016617c7267ef
WasabiMushyPeas <54775398+WasabiMushyPeas@users.noreply.github.com>
2022-05-11 19:25:22 +0100

    Filling in
    Added a Pos class to manage piece positions and worked on the board  class integrating the Pos class.

--- src/Board.java
+++ src/Board.java
@@ -3,6 +3,45 @@ public class Board {
     private Piece[][] board = new Piece[8][8];
 
     public Board(){
+        board = setupBoard();
+    }
+
+    public Piece[][] getBoard(){
+        return board;
+    }
+
+    public Piece[] getBoard(int row){
+        return board[row];
+    }
+
+    public Piece getBoard(Pos pos){
+        return board[pos.getRow()][pos.getColum()];
+    }
+
+    public Piece[][] setPos(Piece piece, Pos OldPos, Pos NewPos){
+        board[OldPos.getRow()][OldPos.getColum()] = null;
+        board[NewPos.getRow()][NewPos.getColum()] = piece;
+        return board;
+    }
+
+    public Pos getPosOfPiece(Piece piece){
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board.length; j++){
+                if(board[i][j].equals(piece)){
+                    Pos piecePos = new Pos(i, j);
+                    return piecePos;
+                }
+            }
+        }
+        Pos piecePos = new Pos(-1, -1);
+        return piecePos;
+    }
+
+
+    private Piece[][] setupBoard(){
+        Piece[][] tempBoard = new Piece[8][8];
         
+
+        return tempBoard;
     }
 }
---
+++ src/Pos.java
@@ -0,0 +1,49 @@
+package src;
+
+public class Pos {
+    private int row;
+    private int colum;
+
+    public Pos(){
+        row = 0;
+        colum = 0;
+    }
+
+    public Pos(int row, int colum){
+        this.row = row;
+        this.colum = colum;
+    }
+
+    public int[] getPos(){
+        int[] pos = {row, colum};
+        return pos;
+    }
+
+    public int getRow(){
+        return row;
+    }
+
+    public int getColum(){
+        return colum;
+    }
+
+    public void setPos(int[] RowColum){
+        row = RowColum[0];
+        colum = RowColum[1];
+    }
+
+    public void setPos(int row, int colum){
+        this.row = row;
+        this.colum = colum;
+    }
+
+    public void setRow(int row){
+        this.row = row;
+    }
+
+    public void setColum(int colum){
+        this.colum = colum;
+    }
+
+
+}

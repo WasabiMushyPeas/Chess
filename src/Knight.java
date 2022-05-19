2f02b7c6d9a4412161e86aa3b9273e1d1a8bf5ac
WasabiMushyPeas <54775398+WasabiMushyPeas@users.noreply.github.com>
2022-05-19 18:15:20 +0100

    Knight
    the knight now moves correctly and it works.

--- src/Knight.java
+++ src/Knight.java
@@ -1,7 +1,41 @@
 package src;
+
 public class Knight extends Piece {
-    public Knight(Pos pos, Boolean isWhite){
+    public Knight(Pos pos, Boolean isWhite) {
         super(pos, 3, isWhite, "Knight", 'k');
     }
-    
+
+    public Boolean move(Pos moveTo) {
+        int moveToRow = moveTo.getRow();
+        int moveToColum = moveTo.getColum();
+        int currentRow = this.getPos().getRow();
+        int currentColum = this.getPos().getColum();
+
+        if ((currentRow + 1 == moveToRow && currentColum + 2 == moveToColum)
+                || (currentRow - 1 == moveToRow && currentColum - 2 == moveToColum)
+                || (currentRow + 1 == moveToRow && currentColum - 2 == moveToColum)
+                || (currentRow - 2 == moveToRow && currentColum - 1 == moveToColum)
+                || (currentRow + 2 == moveToRow && currentColum - 1 == moveToColum)
+                || (currentRow + 2 == moveToRow && currentColum + 1 == moveToColum)
+                || (currentRow + 1 == moveToRow && currentColum + 2 == moveToColum)
+                || (currentRow - 1 == moveToRow && currentColum + 2 == moveToColum)
+                || (currentRow - 2 == moveToRow && currentColum + 1 == moveToColum)) {
+
+                    if(Main.chessBoard.getBoard(moveTo) == null){
+                        super.moved();
+                        Main.chessBoard.setPos(this, this.getPos(), moveTo);
+                        return true;
+                    }else{
+                        if(Main.chessBoard.getBoard(moveTo).isWhite() != this.isWhite())
+                        Main.chessBoard.takePos(moveTo, this.isWhite());
+                        super.moved();
+                        Main.chessBoard.setPos(this, this.getPos(), moveTo);
+                        return true;
+                    }
+
+        }
+        return false;
+
+    }
+
 }
